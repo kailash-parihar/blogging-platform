a8f359f7aa6dd22ac3a658a41a991b22b56b6f73
Kailash Parihar <kailashparihar1999@gmail.com>
2025-03-09 12:17:43 +0530

    update the login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,30 +1,55 @@
+const bcrypt = require("bcrypt");
 const { User } = require("../models/user.model");
 
 // User Registration Controller
-const signupUser = (req, res) => {
-  User.create(req.body)
-    .then((response) => {
-      res.status(201).json({
-        status: "success",
-        message: "User successfully registered. Welcome!",
-        data: {
-          user: {
-            id: newUser._id,
-            email: newUser.email,
-          },
-        },
-      });
-    })
-    .catch((error) => {
-      console.error("User registration failed:", error);
-      res
-        .status(500)
-        .json({ Message: "User registration failed", error: error });
+const signupUser = async (req, res) => {
+  try {
+    let { firstName, lastName } = await User.create(req.body);
+    res.status(201).json({
+      status: "success",
+      message: `Congratulations ${firstName} ${lastName} ! Your account has been successfully created.`,
     });
+  } catch (error) {
+    res.status(500).json({
+      status: "User registration failed",
+      error: error.name,
+      message: error.message,
+    });
+  }
 };
 
 // User Login Controller
-const loginUser = () => {};
+const loginUser = async (req, res) => {
+  try {
+    let { email, username, password } = req.body;
+
+    let user = await User.findOne({
+      $or: [{ email: email }, { username: username }],
+    });
+
+    if (!user) {
+      res.status(401).json({ message: "User not found!" });
+    } else {
+      const hashedPassword = user.password;
+      const isPasswordMatch = await bcrypt.compare(password, hashedPassword);
+
+      if (!isPasswordMatch) {
+        return res.status(401).json({ message: "Invalid Password" });
+      }
+
+      res.status(200).json({
+        status: "success",
+        message: `Welcome back, ${user.firstName} ${user.lastName}! You have logged in successfully.`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      Message: "Something went wrong",
+      error: error.name,
+      error: error.message,
+    });
+  }
+};
 
 // User Logout Controller
 const logoutUser = () => {};
@@ -35,10 +60,14 @@ const forgotUserPassword = () => {};
 // User Password Reset Controller
 const resetUserPassword = () => {};
 
+// Delete User Controller
+const deleteUser = () => {};
+
 module.exports = {
   signupUser,
   loginUser,
   logoutUser,
   forgotUserPassword,
   resetUserPassword,
+  deleteUser,
 };
