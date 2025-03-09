00d9ef3c702fceee6772ce897e7b6e188f65492d
Kailash Parihar <kailashparihar1999@gmail.com>
2025-03-09 12:22:54 +0530

    adding deleteUser Routes

--- backend/src/routers/auth.routes.js
+++ backend/src/routers/auth.routes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const authRouter = express.Router();
 
-const { signupUser, loginUser, logoutUser, forgotUserPassword, resetUserPassword } = require("../controllers/auth.controller");
+// Importing Controller for these endpoints
+const {
+  signupUser,
+  loginUser,
+  logoutUser,
+  forgotUserPassword,
+  resetUserPassword,
+  deleteUser,
+} = require("../controllers/auth.controller");
 
 authRouter.post("/signup", signupUser);
 authRouter.post("/login", loginUser);
 authRouter.post("/logout", logoutUser);
 authRouter.post("/forgot-password", forgotUserPassword);
 authRouter.post("/reset-password", resetUserPassword);
+authRouter.post("/delete-user", deleteUser);
 
 module.exports = { authRouter };
