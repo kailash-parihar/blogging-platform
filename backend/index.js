8f6f99cac59864b497c4acbdf637a7f4fc1f4667
Kailash Parihar <kailashparihar1999@gmail.com>
2025-03-09 11:03:38 +0530

    remove the root endpoint controller and added more custom routes

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,33 @@
-const express = require("express");
-const app = express();
+/**
+ * Main entry point for the application.
+ * This script sets up the Express server, configures middleware,
+ * initializes the database connection, and defines the routing endpoints.
+ */
 
+const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-const port = process.env.PORT || 3000;
 
-const { dbConfig } = require("./src/configurations/db.config");
-const { authRouter } = require("./src/routers/auth.routes");
-
-app.get("/", (req, res) => {
-  res.send(`<div style="text-align: center;"><h1>Welcome, Kailash</h1></div>`);
-});
-
+const app = express();
 app.use(express.json());
-app.use("/api/v1/auth", authRouter);
 
+// Initialize database connection
+const { dbConfig } = require("./src/configurations/db.config.js");
+
+// Import routes
+const { homeRouter } = require("./src/routers/home.routes.js");
+const { authRouter } = require("./src/routers/auth.routes");
+const { userRouter } = require("./src/routers/user.routes.js");
+const { articleRouter } = require("./src/routers/article.routes");
+
+// Define application routes
+app.get("/", homeRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/article", articleRouter);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  dbConfig();
-  console.log(`\nServer is start running on http://127.0.0.1:${port}/`);
+  dbConfig(); // Establish connection to the database
+  console.log(`Server is start running on http://127.0.0.1:${port}/`);
 });
